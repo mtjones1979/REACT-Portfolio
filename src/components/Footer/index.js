7cd6c11577b346b3de766ca833d2ecb22bee3f95
Michael Jones <mtjones1979@yahoo.com>
2021-05-16 17:50:45 -0500

    set up folders, footer frame

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer class="footer">
+        <span>MTJ Production | Privacy | Terms</span>
+    </footer>
+  );
+}
+
+export default Footer;

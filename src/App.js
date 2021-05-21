3b376880b070e1e43ac4aeadbc7a885b0f8f90c5
Michael Jones <mtjones1979@yahoo.com>
2021-05-21 10:01:36 -0500

    set up header/navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
+import { BrowserRouter as Router } from "react-router-dom";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Portfolio from "./pages/Portfolio";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -11,13 +11,13 @@ function App() {
   return (
     <Router>
       <div>
-      <Header / >
-        <Switch>
+      <Header />
+        {/* <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
-        </Switch>
+        </Switch> */}
       <Footer />
       </div>
     </Router>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,37 @@
 import React from "react";
-// import "./style.css";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 function Header () {
-    return (
-     
-  <nav class="navbar sticky-top navbar-expand-lg navbar-dark px-3" style="background-color: black">
-  <a class="navbar-brand" href="/">Michael Thomas Jones</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  return (
+    <nav className="navbar  navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Michael Thomas Jones
+      </Link>
+    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false">
+      <span className="navbar-toggler-icon"></span>
+    </button> */}
 
-  <div class="collapse navbar-collapse justify-content-end" id="navbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="index.html">About</a>
+  <div className="collapse navbar-collapse justify-content-end" id="navbar">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link to="/"
+          className={window.location.pathname === "/" || window.location.pathname === "/about"
+          ? "nav-link active" : "nav-link"}>
+          About
+        </Link>
       </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="portfolio.html">Portfolio</a>
+      <li className="nav-item">
+        <Link to="/portfolio" 
+          className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+          Portfolio
+        </Link>
       </li>
-        <li class="nav-item">
-        <a class="nav-link active" href="contact.html">Contact</a>
+        <li className="nav-item">
+        <Link to="/contact" 
+          className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+          Contact
+        </Link>
       </li>
     </ul>
   </div>  
